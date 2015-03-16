9ec8db5f5bbbed497040caef5cbed808c7be9a10
titiyo <bondeau.yohann@gmail.com>
2015-03-16 14:48:13 +0100

    End of serverQueryResponseParser

--- CSGOManager_Server/tools/serverQueryResponseParser.js
+++ CSGOManager_Server/tools/serverQueryResponseParser.js
@@ -27,35 +27,151 @@ function ServerQueryResponseParser(buffer){
 };
 
 function ParseA2SInfoResponse(buffer){
+	var offset = 8;
+	this.protocol = buffer.readUInt8LE(offset);
+	offset += 8;
+	this.name = this.getNextString(buffer, offset);
+	offset += this.name.length;
+	this.map = this.getNextString(buffer, offset);
+	offset += this.map.length;
+	this.folder = this.getNextString(buffer, offset);
+	offset += this.folder.length;
+	this.game = this.getNextString(buffer, offset);
+	offset += this.game.length;
+	this.id = buffer.readUInt16LE(offset);
+	offset += 16;
+	this.players = buffer.readUInt8LE(offset);
+	offset += 8;
+	this.maxPlayers = buffer.readUInt8LE(offset);
+	offset += 8;
+	this.bots = buffer.readUInt8LE(offset);
+	offset += 8;
+	this.serverTypeTmp = buffer.readUInt8LE(offset);
+	offset += 8;
+	if(this.serverTypeTmp === 'd'){
+		this.serverType = "Dedicated server";
+	}else if (this.serverTypeTmp === 'l'){
+		this.serverType = "Non-dedicated server";
+	}
+	else if(this.serverTypeTmp === 'p'){
+		this.serverType = "SourceTV ";
+	}else {
+		this.serverType = "Erreur : Impossible de parser le type de serveur !";
+	}
+	this.environmentTmp = buffer.readUInt8LE(offset);
+	offset += 8;
+	if(this.environmentTmp === 'l'){
+		this.environment = "Linux";
+	}else if (this.environmentTmp === 'w'){
+		this.environment = "Windows";
+	}
+	else if(this.environmentTmp === 'm' || this.environmentTmp === 'o'){
+		this.environment = "Mac";
+	}else {
+		this.environment = "Erreur : Impossible de parser l'environment !";
+	}
+	this.visibilityTmp = buffer.readUInt8LE(offset);
+	offset += 8;
+	if(this.visibilityTmp === 0){
+		this.visibility = "Public";
+	}else if (this.visibilityTmp === 1){
+		this.visibility = "Private";
+	}else {
+		this.visibility = "Erreur : Impossible de parser la visibilité du serveur !";
+	}
+	this.vacTmp = buffer.readUInt8LE(8);
+	offset += 8;
+	if(this.vacTmp === 0){
+		this.vac = "Unsecured";
+	}else if (this.vacTmp === 1){
+		this.vac = "Secured";
+	}else {
+		this.vac = "Erreur : Impossible de parser la sécurité du serveur !";
+	}
+	this.version = this.getNextString(buffer, offset);
+	offset += this.version.length;
+	if(buffer.length > offset){
+		this.extraDataFlag = buffer.readUInt8LE(8);
+		offset += 8;
+		
+		if(this.extraDataFlag & 0x80){
+			this.port = buffer.readUInt16LE(offset);
+			offset += 16;
+		}
+		if(this.extraDataFlag & 0x10){
+			this.steamId = buffer.readDoubleLE(offset);
+			offset += 64;
+		}
+		if(this.extraDataFlag & 0x40){
+			this.portSourceTV = buffer.readUInt16LE(offset);
+			offset += 16;
+			this.sourceTvName = this.getNextString(buffer, offset);
+			offset += this.sourceTvName.length;
+		}
+	}
+
 	return {
-		Protocol: buffer.readUInt8LE(8),
-		Name: buffer.readUInt8LE(8),
-		Map: buffer.readUInt8LE(8),
-		Folder: buffer.readUInt8LE(8),
-		Game: buffer.readUInt8LE(8),
-		ID: buffer.readUInt8LE(8),
-		Players: buffer.readUInt8LE(8),
-		MaxPlayers : buffer.readUInt8LE(8),
-		Bots: buffer.readUInt8LE(8),
-		ServerType : buffer.readUInt8LE(8),
-		Environment: buffer.readUInt8LE(8),
-		Visibility: buffer.readUInt8LE(8),
-		VAC: buffer.readUInt8LE(8),
-		Version: buffer.readUInt8LE(8),
-		ExtraDataFlag: buffer.readUInt8LE(8)
+		Protocol: this.protocol,
+		Name: this.name,
+		Map: this.map,
+		Folder: this.folder,
+		Game: this.game,
+		ID: this.id,
+		Players: this.players,
+		MaxPlayers : this.maxPlayers,
+		Bots: this.bots,
+		ServerType : this.serverType,
+		Environment: this.environment,
+		Visibility: this.visibility,
+		VAC: this.vac,
+		Version: this.version,
+		port: this.port != undefined ? this.port : null,
+		steamId: this.steamId != undefined ? this.steamId : null,
+		portSourceTV: this.portSourceTV != undefined ? this.portSourceTV : null,
+		sourceTvName: this.sourceTvName != undefined ? this.sourceTvName : null		
 	};
 }
 
 function ParseA2SPlayerResponse(buffer){
+	var i = 0;
+	var offset = 8;
+	this.numberPlayer = buffer.readUInt8LE(offset);
+	offset += 8;
+	this.players = [];
 	
+	for(i=0; i < this.numberPlayer; i++){
+		var index = buffer.readUInt8LE(offset);
+		offset += 8;
+		var name = this.getNextString(buffer, offset);
+		offset += name.length;
+		var score = buffer.readUInt32LE(offset);
+		offset += 32;
+		var duration = buffer.readFloatLE(offset);
+		offset += 32;
+		this.players.push({Index: index, Name : name, Score: score, Duration: duration});
+	}
+	return {
+		NumberPlayer: this.numberPlayer,
+		Players: this.players	
+	};
 }
 
 function ParseA2PingResponse(buffer){
-	
+	return {Payload : this.getNextString(buffer, 8)};
 }
 
 function ParseA2SServerQueryGetChallengeResponse(buffer){
-	
+	return {Challenge : buffer.readUInt32LE(8)};
+}
+
+function getNextString(buffer, offset){
+	var str = "";
+	var i = offset;
+	while(buffer[i] != 0x00){
+		str += buffer[i];
+		i++;
+	}
+	return str;
 }
 
 ServerQueryResponseParser.A2SInfoResponseHeader = 0x49;
